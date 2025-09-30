affb2c6e2150df0509fda5eeabe166fd60311510
Matthias Archut <matthias.archut@deutschebahn.com>
2025-09-30 16:27:00 +0200

    upd lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,18 +8,132 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"mason-org/mason-lspconfig.nvim",
+			-- "neovim/nvim-lspconfig",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
 		config = function()
-			vim.lsp.config("lua_ls", {
-				settings = {
-					Lua = {
-						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true),
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+					-- "prettierd",
+					-- "eslint-lsp",
+					-- "tailwindcss-language-server",
+					-- "ts_ls",
+					-- "graphql",
+					"json-lsp",
+					"lua_ls",
+					"prettier",
+					"pyright",
+					"robotcode",
+					"xmlformatter",
+					-- "yang_lsp",
+				},
+				auto_update = false,
+				run_on_start = true,
+			})
+
+			-- vim.lsp.config("lua_ls", {
+			-- 	settings = {
+			-- 		Lua = {
+			--          runtime = {
+			--            -- Tell the language server which version of Lua you're using
+			--            -- (most likely LuaJIT in the case of Neovim)
+			--            version = "LuaJIT",
+			--          },
+			--          diagnostics = {
+			--            -- Get the language server to recognize the `vim` global
+			--            globals = {
+			--              "vim",
+			--              "require",
+			--            },
+			--          },
+			--          workspace = {
+			--          -- Make the server aware of Neovim runtime files
+			--            library = vim.api.nvim_get_runtime_file("", true),
+			--          },
+			--          -- Do not send telemetry data containing a randomized but unique identifier
+			--          telemetry = {
+			--            enable = false,
+			--          },
+			--        },
+			--      },
+			-- })
+
+			local lsps = {
+				{
+					"lua_ls",
+					{
+						cmd = { "lua-language-server" },
+						filetypes = { "lua" },
+						root_markers = {
+							".luarc.json",
+							".luarc.jsonc",
+							".luacheckrc",
+							".stylua.toml",
+							"stylua.toml",
+							"selene.toml",
+							"selene.yml",
+							".git",
+						},
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
 						},
 					},
 				},
-			})
+				{
+					"pyright",
+					{
+						cmd = { "pyright-langserver", "--stdio" },
+						filetypes = { "python" },
+						root_markers = {
+							"pyproject.toml",
+							"setup.py",
+							"setup.cfg",
+							"requirements.txt",
+							"Pipfile",
+							"pyrightconfig.json",
+							".git",
+						},
+						settings = {
+							python = {
+								analysis = {
+									autoSearchPaths = true,
+									useLibraryCodeForTypes = true,
+									diagnosticMode = "openFilesOnly",
+								},
+							},
+						},
+					},
+				},
+				{
+					"robotcode",
+					{
+						cmd = { "robotcode", "language-server" },
+						filetypes = { "robot", "resource" },
+						root_markers = { "robot.toml", "pyproject.toml", "Pipfile", ".git" },
+						get_language_id = function(_, _)
+							return "robotframework"
+						end,
+					},
+				},
+			}
 
-			vim.lsp.enable("lua_ls")
+			for _, lsp in pairs(lsps) do
+				local name, config = lsp[1], lsp[2]
+				vim.lsp.enable(name)
+				if config then
+					vim.lsp.config(name, config)
+				end
+			end
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -33,7 +147,7 @@ return {
 					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
 					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "<leader>f", function()
+					vim.keymap.set("n", "<leader>fu", function()
 						vim.lsp.buf.format({ timeout_ms = 10000 })
 					end)
 
